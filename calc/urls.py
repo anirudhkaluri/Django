08f1d60a339984c44f3e0ca4effecdb66aa2aa42
anirudhkaluri <anirudhkaluri@gmail.com>
2023-03-10 17:26:18 +0530

    .gitignore added

--- calc/urls.py
+++ calc/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 
 from . import views
 
-url_pattern=[ #this will be list
-    path('/',views.home,name="home") #but we dont have this funciton
+urlpatterns=[ #this will be list
+    path('',views.home,name="home") #but we dont have this funciton
+    #since this is home page dont metion / or anything like that
     #path takes two arguments 1) url patter 2) a view function that is called when url is matched
     #in django we can assign a name to each url pattern
 ]
