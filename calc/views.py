e15607c23a2773c12c63b62f8ee279e12396086d
anirudhkaluri <anirudhkaluri@Anirudh-Kaluris-Mac.local>
2023-03-16 15:52:52 +0530

    implemented add with get

--- calc/views.py
+++ calc/views.py
@@ -10,3 +10,10 @@ def home(request):
     #return HttpResponse("<h1>Hello World</h1>") this will work
     #return render(request,'home.html') #this home.html is in templates folder 
     return render(request,'home.html',{'name':'anirudh'})
+
+def add(request):
+    val1=request.GET['number1']
+    val2=request.GET['number2']
+    #make sure you use single quote above
+    res=int(val1) + int(val2)
+    return render(request,'result.html',{'result':res})
